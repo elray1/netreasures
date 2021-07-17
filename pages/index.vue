6ad5bb3f14b32acf6f9369b80c9bfd9c769457b2
Evan Ray <elray@umass.edu>
2021-07-17 14:14:59 -0400

    remove dist build on main branch, remove unused Logo component and main.js files

--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,43 @@
 <template>
   <div id="app">
-    <vl-map
-      :load-tiles-while-animating="true"
-      :load-tiles-while-interacting="true"
-      data-projection="EPSG:4326"
-      style="height: 100%;">
+    <client-only>
+      <vl-map
+        :load-tiles-while-animating="true"
+        :load-tiles-while-interacting="true"
+        data-projection="EPSG:4326"
+        style="height: 100%;">
 
-      <vl-view :zoom.sync="zoom" :center.sync="center" :rotation.sync="rotation"></vl-view>
+        <vl-view :zoom.sync="zoom" :center.sync="center" :rotation.sync="rotation"></vl-view>
 
-      <vl-geoloc @update:position="geolocPosition = $event">
-        <template slot-scope="geoloc">
-          <vl-feature v-if="geoloc.position" id="position-feature">
-            <vl-geom-point :coordinates="geoloc.position"></vl-geom-point>
+        <vl-geoloc @update:position="geolocPosition = $event">
+          <template slot-scope="geoloc">
+            <vl-feature v-if="geoloc.position" id="position-feature">
+              <vl-geom-point :coordinates="geoloc.position"></vl-geom-point>
+              <vl-style-box>
+                <vl-style-icon :src="require('~/assets/marker-red.png')" :scale="0.3" :anchor="[0.5, 1]"></vl-style-icon>
+              </vl-style-box>
+            </vl-feature>
+          </template>
+        </vl-geoloc>
+
+        <vl-feature id="marker" ref="marker">
+          <template slot-scope="feature">
+            <vl-geom-point :coordinates="[-10, -10]"></vl-geom-point>
             <vl-style-box>
-              <vl-style-icon :src="require('~/assets/marker-red.png')" :scale="0.3" :anchor="[0.5, 1]"></vl-style-icon>
+              <vl-style-icon :src="require('../assets/marker-blue.png')" :scale="0.5" :anchor="[0.1, 0.95]" :size="[128, 128]"></vl-style-icon>
             </vl-style-box>
-          </vl-feature>
-        </template>
-      </vl-geoloc>
+            <!-- overlay binded to feature -->
+          </template>
+        </vl-feature>
 
-      <vl-feature id="marker" ref="marker">
-        <template slot-scope="feature">
-          <vl-geom-point :coordinates="[-10, -10]"></vl-geom-point>
-          <vl-style-box>
-            <vl-style-icon :src="require('../assets/marker-blue.png')" :scale="0.5" :anchor="[0.1, 0.95]" :size="[128, 128]"></vl-style-icon>
-          </vl-style-box>
-          <!-- overlay binded to feature -->
-        </template>
-      </vl-feature>
-
-      <vl-layer-tile id="osm">
-        <vl-source-osm></vl-source-osm>
-      </vl-layer-tile>
-    </vl-map>
+        <vl-layer-tile id="osm">
+          <vl-source-osm></vl-source-osm>
+        </vl-layer-tile>
+      </vl-map>
+    </client-only>
   </div>
 </template>
 
-<script src="./main.js"></script>
 <script>
 export default {
   data () {
